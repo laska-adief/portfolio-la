5ec378a96a26ada2f404afe62c221f46792fbb5b
laska-adief <laskaadief.la@gmail.com>
2023-04-03 15:59:44 +0700

    [Portfolio LA] add interfaces

---
+++ src/app/interfaces/education.ts
@@ -0,0 +1,9 @@
+export interface Education {
+  id: string;
+  title: string;
+  university: string;
+  startDate: string;
+  finishDate: string;
+  gpa: string;
+  location: string;
+}
---
+++ src/app/interfaces/job.ts
@@ -0,0 +1,8 @@
+export interface Job {
+  id: string;
+  title: string;
+  company: string;
+  startDate: string;
+  finishDate: string;
+  location: string;
+}
