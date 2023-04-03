c564149114b787d4ef413a09783335276982b514
laska-adief <laskaadief.la@gmail.com>
2023-04-03 18:34:44 +0700

    [Portfolio LA] Switching theme and icon brand

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portfolio-la';
+  theme: 'dark' | 'light' = 'dark';
+
+  getTheme(selectedTheme: boolean) {
+    if (selectedTheme) {
+      this.theme = 'dark';
+    } else {
+      this.theme = 'light';
+    }
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  faBars,
+  faTimes,
+  faSun,
+  faMoon,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
@@ -7,15 +12,24 @@ import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() selectionTheme = new EventEmitter();
   constructor() {}
 
   faBars = faBars;
   faTimes = faTimes;
+  faSun = faSun;
+  faMoon = faMoon;
   isMenuOpen: boolean = false;
+  isDarkTheme: boolean = true;
 
   ngOnInit(): void {}
 
   onClickMenuIcon() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    this.selectionTheme.emit(this.isDarkTheme);
   }
 }
