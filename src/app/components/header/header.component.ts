cf330788ebf398b2dbdb03368b81f3663c829689
laska-adief <laskaadief.la@gmail.com>
2023-04-03 16:00:26 +0700

    [Portfolio LA] create header compnent

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  faBars = faBars;
+  faTimes = faTimes;
+  isMenuOpen: boolean = false;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClickMenuIcon() {
+    this.isMenuOpen = !this.isMenuOpen;
   }
-
 }
