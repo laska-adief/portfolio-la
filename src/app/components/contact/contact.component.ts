127d188be088a64bed0c31264899335e7a6013d1
laska-adief <laskaadief.la@gmail.com>
2023-04-03 16:02:32 +0700

    [Portfolio LA] create contact component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
+  name = '';
+  email = '';
+  subject = '';
+  message = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  sendMessage() {
+    var mail = `mailto:laskaadief.la@gmail.com?subject=${this.subject}&body=${this.message}`;
+    console.log(mail);
+
+    var a = document.createElement('a');
+    a.href = mail;
+    a.click();
   }
-
 }
